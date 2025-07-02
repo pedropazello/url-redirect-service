24fec8a5f922df9dea9469ade960b72b125286db
Pedro Pazello <email@pedropazello.com>
2025-07-01 21:37:07 -0300

    Add test for error context in DynamoDB

--- infra/db/dynamoDB_test.go
+++ infra/db/dynamoDB_test.go
@@ -2,6 +2,7 @@ package db_test
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -12,16 +13,23 @@ import (
 	"github.com/pedropazello/url-redirect-service/mocks"
 )
 
-func TestGetItem_Success(t *testing.T) {
-	clientMock := mocks.IDynamodbClient{}
-	dynamoDB := db.NewDynamoDB(&clientMock)
+var clientMock mocks.IDynamodbClient
+var dynamoDB *db.DynamoDB
+var input *dynamodb.GetItemInput
 
-	input := &dynamodb.GetItemInput{
+func setup(_ *testing.T) {
+	clientMock = mocks.IDynamodbClient{}
+	dynamoDB = db.NewDynamoDB(&clientMock)
+	input = &dynamodb.GetItemInput{
 		TableName: aws.String("Redirects"),
 		Key: map[string]types.AttributeValue{
 			"Id": &types.AttributeValueMemberS{Value: "1"},
 		},
 	}
+}
+
+func TestGetItem_Success(t *testing.T) {
+	setup(t)
 
 	output := &dynamodb.GetItemOutput{
 		Item: map[string]types.AttributeValue{
@@ -37,4 +45,18 @@ func TestGetItem_Success(t *testing.T) {
 	assert.Equal(t, nil, err)
 	assert.Equal(t, "1", result["Id"].(string))
 	assert.Equal(t, "http://foo.com", result["RedirectToURL"].(string))
+}
+
+func TestGetItem_Error(t *testing.T) {
+	setup(t)
+
+	output := &dynamodb.GetItemOutput{}
+	expectedErr := errors.New("connection error")
+
+	clientMock.EXPECT().GetItem(context.Background(), input).Return(output, expectedErr)
+	result, err := dynamoDB.GetItem(context.Background(), "1")
+
+	assert.Equal(t, expectedErr, err)
+	assert.Equal(t, nil, result["Id"])
+	assert.Equal(t, nil, result["RedirectToURL"])
 }
